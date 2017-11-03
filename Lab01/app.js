9c4f7aad87a93b0bff0832c39cbb9c6001e3e737
volneygs <volneygsilva@gmail.com>
2017-11-03 17:37:58 -0300

    US3 passo 1 finalizado

--- Lab01/app.js
+++ Lab01/app.js
@@ -4,8 +4,12 @@ app.controller("ispotifaiCtrl", function($scope){
 
     $scope.artistas = [];
     $scope.albuns = [];
+    $scope.favoritos = [];
+    $scope.listaArtistaProcurado = [];
+    $scope.artistaProcurado = "";
     $scope.menuArtista = false;
-    $scope.menuMusica= false;
+    $scope.menuMusica = false;
+    $scope.menuListaDeArtistas = false;
 
     $scope.adicionarArtista = function(artista){
 
@@ -26,41 +30,107 @@ app.controller("ispotifaiCtrl", function($scope){
 
     };
 
-    $scope.adicionaMusica = function(musica){
+    $scope.adicionarMusica = function(musica){
 
-      var key = false;
+      var naoAdicionado = true;
 
-      for (var i = 0; i < $scope.musicas.length; i++) {
-        if($scope.albuns[i].nome === musica.album){
-          key = true;
+      if($scope.albuns.length === 0){
+
+        $scope.albuns.push($scope.criaAlbum(musica));
+        alert("Música adicionada com sucesso");
+
+      }else{
+        for (var i = 0; i < $scope.albuns.length; i++) {
+          if($scope.verificaAlbumIgual($scope.albuns[i], musica)){
+            if($scope.verificaMusicaIgual($scope.albuns[i], musica)){
+              alert("Não pode existir duas músicas com o mesmo nome no mesmo álbum");
+            }else{
+              if(naoAdicionado){
+                $scope.albuns[i].musicas.push(musica);
+                alert("Música adicionada com sucesso");
+                key = false;
+              }
+            }
+
+          }else{
+            if(naoAdicionado){
+              $scope.albuns.push($scope.criaAlbum(musica));
+              alert("Música adicionada com sucesso");
+              key = false;
+            }
+          }
         }
       }
 
-      if(key === true){
+      delete $scope.musica;
+    };
 
-      }else{
-        
+    $scope.procuraArtista = function(artista){
+
+      $scope.listaArtistaProcurado = [];
+
+      for (var i = 0; i < artistas.length; i++) {
+        if($scope.artistas[i].nome === artista.nome){
+          $scope.listaArtistaProcurado.push($scope.artistas[i]);
+        }
       }
 
+    }
+
+    $scope.adicionaListaDeFavoritos = function (artista){
+
+      $scope.favoritos.push(artista);
+      delete $scope.artista;
     }
 
     $scope.criaAlbum = function(musica){
 
-      var album;
-      album.nome = musicas.album;
+      var album = new Object();
+      album.nome = musica.album;
+      album.musicas = [];
+      album.musicas.push(musica);
 
       return album;
 
-    }
+    };
+
+    $scope.verificaAlbumIgual = function(album, musica){
+
+      if(album.nome === musica.album){
+        return true;
+      }
+
+      return false;
+
+    };
+
+    $scope.verificaMusicaIgual = function(album, musica){
+
+      for (var i = 0; i < album.musicas.length; i++) {
+        if(album.musicas[i].nome === musica.nome){
+          return true;
+        }
+      }
+
+      return false;
+    };
 
     $scope.alteraMenu = function(number){
 
       if(number === 1){
         $scope.menuArtista = true;
         $scope.menuMusica = false;
+        $scope.menuListaDeArtistas = false;
       }else if (number === 2) {
         $scope.menuMusica = true;
         $scope.menuArtista = false;
+        $scope.menuListaDeArtistas = false;
+
+      }else if (number === 3) {
+        $scope.menuListaDeArtistas = true;
+        $scope.menuMusica = false;
+        $scope.menuArtista = false;
+
       }
 
     };
