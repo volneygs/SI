ca10fa328d3dc419db9096a494fd147804d4fd6b
volneygs <volneygsilva@gmail.com>
2017-11-09 03:59:41 -0300

    lab01 finalizado

--- Lab01/app.js
+++ Lab01/app.js
@@ -6,11 +6,66 @@ app.controller("ispotifaiCtrl", function($scope){
     $scope.albuns = [];
     $scope.favoritos = [];
     $scope.listaArtistaProcurado = [];
+    $scope.playlists = [];
     $scope.artistaProcurado = "";
     $scope.artistaModal = "";
+    $scope.playlistModal = "";
     $scope.menuArtista = false;
     $scope.menuMusica = false;
     $scope.menuListaDeArtistas = false;
+    $scope.menuHome = false;
+    $scope.menuFavoritos = false;
+    $scope.menuNovaPlaylist = false;
+    $scope.menuPlaylists = false;
+
+    $scope.adicionaMusicaPlaylist = function(musicaAdd){
+      $scope.playlistModal.musicas.push(musicaAdd);
+
+      delete $scope.musicaAdd;
+    }
+
+    $scope.removeMusicaPlaylist = function(musicaPlaylist){
+
+      var naoExcluido = true;
+
+      for (var i = $scope.playlistModal.musicas.length -1; i >= 0; i--) {
+        if($scope.playlistModal.musicas[i].nome === musicaPlaylist.nome && naoExcluido){
+          $scope.playlistModal.musicas.splice(i,1);
+          naoExcluido = false;
+        }
+      }
+    }
+
+    $scope.criaPlaylist = function(playlist){
+
+      var naoIguais = true;
+
+      for (var i = 0; i < $scope.playlists.length; i++) {
+        if($scope.playlists[i].nome === playlist.nome && naoIguais){
+          naoIguais = false;
+          alert("Você não pode criar duas playlists com o mesmo nome!")
+        }
+      }
+
+      if(naoIguais){
+        playlist.musicas = [];
+        $scope.playlists.push(playlist);
+      }
+
+      delete $scope.playlist;
+    };
+
+    $scope.removePlaylist = function(playlist){
+
+      var naoRemovido = true;
+
+      for(var i = $scope.playlists.length-1; i >= 0; i--){
+        if ($scope.playlists[i].nome === playlist.nome && naoRemovido){
+          $scope.playlists.splice(i,1);
+          naoRemovido = false;
+        }
+      }
+    };
 
     $scope.adicionarNota = function(artista, nota){
       artista.nota = nota;
@@ -31,6 +86,7 @@ app.controller("ispotifaiCtrl", function($scope){
       if(key === false){
 
         artista.albuns = [];
+        artista.ultimaMusica = "Nenhuma música ouvida!"
 
         for (var i = 0; i < $scope.albuns.length; i++) {
           if ($scope.albuns[i].artista === artista.nome){
@@ -46,6 +102,10 @@ app.controller("ispotifaiCtrl", function($scope){
 
     $scope.menuArtistaModal = function(artistaProcurado){
       $scope.artistaModal = artistaProcurado;
+    }
+
+    $scope.menuPlaylistModal = function(playlistSelecionada){
+      $scope.playlistModal = playlistSelecionada;
     }
 
 
@@ -91,6 +151,7 @@ app.controller("ispotifaiCtrl", function($scope){
 
       $scope.listaArtistaProcurado = [];
 
+
       for (var i = 0; i < $scope.artistas.length; i++) {
         if($scope.artistas[i].nome.indexOf(procurado.nome) !== -1){
           $scope.listaArtistaProcurado.push($scope.artistas[i]);
@@ -103,10 +164,32 @@ app.controller("ispotifaiCtrl", function($scope){
 
     $scope.adicionaListaDeFavoritos = function (artista){
 
-      $scope.favoritos.push(angular.copy(artista));
+      var jaAdicionado = true;
 
-      alert("Artista adicionado com sucesso!")
+      for (var i = 0; i < $scope.favoritos.length; i++) {
+        if ($scope.favoritos[i].nome === artista.nome && jaAdicionado) {
+          alert("Artista já foi adicionado a lista de favoritos!");
+          jaAdicionado = false;
 
+        }
+      }
+
+      if(jaAdicionado){
+        $scope.favoritos.push(angular.copy(artista));
+        alert("Artista adicionado com sucesso!");
+      }
+    }
+
+    $scope.excluirDaListaDeFavoritos = function(artista){
+
+      var naoExcluido = true;
+
+      for (var i = $scope.favoritos.length -1; i >= 0; i--) {
+        if($scope.favoritos[i].nome === artista.nome && naoExcluido){
+          $scope.favoritos.splice(i,1);
+          naoExcluido = false;
+        }
+      }
     }
 
     $scope.criaAlbum = function(musica){
@@ -140,26 +223,77 @@ app.controller("ispotifaiCtrl", function($scope){
         }
       }
 
+
+
       return false;
     };
 
     $scope.alteraMenu = function(number){
 
-      if(number === 1){
+      if(number === 0) {
+        $scope.menuHome = true;
+        $scope.menuArtista = false;
+        $scope.menuMusica = false;
+        $scope.menuListaDeArtistas = false;
+        $scope.menuFavoritos = false;
+        $scope.menuNovaPlaylist = false;
+        $scope.menuPlaylists = false;
+
+      }else if(number === 1){
         $scope.menuArtista = true;
         $scope.menuMusica = false;
         $scope.menuListaDeArtistas = false;
+        $scope.menuHome = false;
+        $scope.menuFavoritos = false;
+        $scope.menuNovaPlaylist = false;
+        $scope.menuPlaylists = false;
+
       }else if (number === 2) {
         $scope.menuMusica = true;
         $scope.menuArtista = false;
         $scope.menuListaDeArtistas = false;
+        $scope.menuHome = false;
+        $scope.menuFavoritos = false;
+        $scope.menuNovaPlaylist = false;
+        $scope.menuPlaylists = false;
 
       }else if (number === 3) {
         $scope.menuListaDeArtistas = true;
         $scope.menuMusica = false;
         $scope.menuArtista = false;
+        $scope.menuHome = false;
+        $scope.menuFavoritos = false;
+        $scope.menuNovaPlaylist = false;
+        $scope.menuPlaylists = false;
 
+      }else if (number === 4){
+        $scope.menuFavoritos = true;
+        $scope.menuListaDeArtistas = false;
+        $scope.menuMusica = false;
+        $scope.menuArtista = false;
+        $scope.menuHome = false;
+        $scope.menuNovaPlaylist = false;
+        $scope.menuPlaylists = false;
+
+      }else if(number ===5){
+        $scope.menuNovaPlaylist = true;
+        $scope.menuFavoritos = false;
+        $scope.menuListaDeArtistas = false;
+        $scope.menuMusica = false;
+        $scope.menuArtista = false;
+        $scope.menuHome = false;
+        $scope.menuPlaylists = false;
+
+      }else if(number === 6){
+        $scope.menuPlaylists = true;
+        $scope.menuHome = false;
+        $scope.menuArtista = false;
+        $scope.menuMusica = false;
+        $scope.menuListaDeArtistas = false;
+        $scope.menuFavoritos = false;
+        $scope.menuNovaPlaylist = false;
       }
+
 
     };
 
@@ -172,5 +306,11 @@ app.controller("ispotifaiCtrl", function($scope){
         }
       }
     };
+
+    $scope.mudaUltimaMusica = function(artistaModal, ultimaMusica){
+      artistaModal.ultimaMusica = ultimaMusica;
+
+      delete $scope.ultimaMusica;
+    }
 
 });
